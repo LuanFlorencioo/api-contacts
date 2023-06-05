9139cfe423b5557373910687df4c6ab7bd89404f
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-05 05:37:25 -0300

    feat(cors): add cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import "express-async-errors";
 import express, { Application } from "express";
 import { handleError } from "./errors";
 import mainRoutes from "./routes/main.routes";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(mainRoutes);
