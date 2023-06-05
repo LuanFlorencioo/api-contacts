6f614ebc1de07536dadd11346ab16ad8e1cc54af
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-05 10:28:19 -0300

    feat(Server): change port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 
 const initServer = () => {
   console.log("~~~ Database is connected ~~~");
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => console.log(`Server is running on ${PORT} 🎉`));
 }
 
